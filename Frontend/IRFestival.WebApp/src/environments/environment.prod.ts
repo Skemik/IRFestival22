2a8f9d0fba174517b0c0687ae63ca2cf461efacc
Skemik <78804840+Skemik@users.noreply.github.com>
2022-10-13 16:24:48 +0200

    Update environment.prod.ts

--- Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   festivalName: "IRFestival",
-  apiBaseUrl: "https://irfestivalapisk.azurewebsites.net/api/"
+  apiBaseUrl: "https://irfestivalapisk.azurewebsites.net/api/",
+  connectionstrings: "InstrumentationKey=1b3bf93f-cc03-4908-b1b0-ea93356e3239;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/"
   
 };
