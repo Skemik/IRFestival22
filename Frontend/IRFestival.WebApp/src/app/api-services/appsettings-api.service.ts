dc3211c89dd9af7afc144084d0784c5ab0b829f3
Skemik <78804840+Skemik@users.noreply.github.com>
2022-10-17 14:22:20 +0200

    Api management

--- Frontend/IRFestival.WebApp/src/app/api-services/appsettings-api.service.ts
+++ Frontend/IRFestival.WebApp/src/app/api-services/appsettings-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Schedule } from '../api/models/schedule.model';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AppSettings } from '../api/models/appsettings.model';
 
@@ -14,6 +14,8 @@ export class AppSettingsApiService {
   constructor(private httpClient: HttpClient) { }
 
   getSettings(): Observable<AppSettings> {
-    return this.httpClient.get<AppSettings>(this.baseUrl);
+    
+    const headers = new HttpHeaders().set("Ocp-Apim-Subscription-Key", "87af1e6f5c8649c59431acd66a26949b");
+    return this.httpClient.get<AppSettings>(this.baseUrl,{'headers':headers});
   }
 }
--- Frontend/IRFestival.WebApp/src/app/api-services/festival-api.service.ts
+++ Frontend/IRFestival.WebApp/src/app/api-services/festival-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Schedule } from '../api/models/schedule.model';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Artist } from '../api/models/artist.model';
 
@@ -14,10 +14,12 @@ export class FestivalApiService {
   constructor(private httpClient: HttpClient) { }
 
   getSchedule(): Observable<Schedule> {
-    return this.httpClient.get<Schedule>(`${this.baseUrl}/lineup`);
+    const headers = new HttpHeaders().set("Ocp-Apim-Subscription-Key", "87af1e6f5c8649c59431acd66a26949b");
+    return this.httpClient.get<Schedule>(`${this.baseUrl}/lineup`, {'headers':headers});
   }
 
   getArtists(): Observable<Artist[]> {
-    return this.httpClient.get<Artist[]>(`${this.baseUrl}/artists`);
+    const headers = new HttpHeaders().set("Ocp-Apim-Subscription-Key", "87af1e6f5c8649c59431acd66a26949b");
+    return this.httpClient.get<Artist[]>(`${this.baseUrl}/artists`,{'headers':headers});
   }
 }
